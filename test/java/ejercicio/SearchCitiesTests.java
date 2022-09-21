3974474dfd0fb6d2fdbc3fa3717e0158c27f6fe5
MiguelAlentado <miguel.alentado@solera.com>
2022-09-21 13:03:54 +0200

    2nd test cities

--- test/java/ejercicio/SearchCitiesTests.java
+++ test/java/ejercicio/SearchCitiesTests.java
@@ -7,15 +7,24 @@ import org.junit.jupiter.api.Test;
 public class SearchCitiesTests {
 	
 	private MockDB database = new MockDB();
-	
+
 	@Test
 	public void find_WhenCall_EmptyArray() {
-		
+
 		SearchCities cities = new SearchCities();
-		
+
 		String [] result = cities.find("d");
-		
+
 		assertEquals(0, result.length);
+	}
+	@Test
+	public void find_WhenCall_EmptyArray() {
+
+		SearchCities cities = new SearchCities();
+
+		String [] result = cities.find("Va");
+
+		assertArray.equals(["Valencia","Valladolid"]);
 	}
 
 }
